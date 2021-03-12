6a55f661b0cf59c061bc33e1f3eb26ee80cba1c2
dp2023 <74738050+dp2023@users.noreply.github.com>
2021-03-12 12:13:40 -0500

    Add files via upload

--- SimpleTakeOffLand.py
+++ SimpleTakeOffLand.py
@@ -3,13 +3,25 @@ from pymavlink import mavutil
 import time
 
 import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument('--connect', default='127.0.0.1:14550')
+parser = argparse.ArgumentParser(description='Commands vehicle using vehicle.simple_goto.')
+parser.add_argument('--connect',
+                    help="Vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
 
+connection_string = args.connect
+sitl = None
+
+
+# Start SITL if no connection string specified
+if not connection_string:
+    import dronekit_sitl
+    sitl = dronekit_sitl.start_default()
+    connection_string = sitl.connection_string()
+
+
 # Connect to the Vehicle
-print 'Connecting to vehicle on: %s' % args.connect
-vehicle = connect(args.connect, baud=57600, wait_ready=True)
+print('Connecting to vehicle on: %s' % connection_string)
+vehicle = connect(connection_string, wait_ready=True)
 
 # Function to arm and then takeoff to a user specified altitude
 def arm_and_takeoff(aTargetAltitude):
