b45ff445d214e5cd6057bebf9b78495248165da7
Charles0208 <cjiang0208@icloud.com>
2022-01-23 15:07:25 -0500

    Fixed Distance to GPS. Theoretically is accurate to the 10th of a millimeter. Yet to test in real life where drone GPS coordinate and altitude will likely be inaccurate
    GPS coords should be accurate to at least millimeter using vincenty's method. Tested by calculating GPS coord and then calculating the distance between drone and GPS coord. This distance and previously calculated distance match up to the  nearest 10000th of a meter when altitude is at 10 meters.
    
    Assumes tilt and distortion has been corrected for previous to running astar.
    
    Theoretically, should be very accurate. Yet to test in real life where drone GPS coordinate and altitude will likely be inaccurate

--- PixelstoCoordinates.py
+++ PixelstoCoordinates.py
@@ -1,4 +1,4 @@
-# Converting pixels of the path into coordinates
+# Converting pixels of the path into coordinates v2
 
 import cv2 as cv
 import numpy as np
@@ -6,44 +6,26 @@ import glob
 import math
 from astar import Node
 
-# Geodesics #May have to change in the future if scale really big. because arc distance ≠ distance calculated using proportions and image points bc it's flat
-# Methods for converting GPS points to distance: https://blog.mapbox.com/fast-geodesic-approximations-with-cheap-ruler-106f229ad016
-# Method with ~0% margin of error for calculating distance given the coordinates at a scale smaller than 100 mi:
-# https://www.govinfo.gov/content/pkg/CFR-2005-title47-vol4/pdf/CFR-2005-title47-vol4-sec73-208.pdf
-# Calculates meters per degree of longitude and latitude based on latitude using above method: https://github.com/mapbox/cheap-ruler-cpp/blob/master/include/mapbox/cheap_ruler.hpp
-# WGS84 ellipsoid model of the Earth: https://ahrs.readthedocs.io/en/latest/wgs84.html
-def MPD(lat_0): 
-    equator_radius = 6378137.0
-    flattening_factor = 1/298.257223563
-    E2 = flattening_factor * (2 - flattening_factor)
-    radians = math.pi/180
-
-
-    mul = math.pi/180 * equator_radius * 1000 # meters per 1 degree longitude at equator
-    w2 = 1 / (1 - E2 * (1 - (math.cos(lat_0*radians)) ** 2)) # I think w2 and w are values that change mul based on latitude
-    w = math.sqrt(w2)
-
-    MPD_long = mul * w * math.cos(lat_0 * radians) # meters per degree longitude
-    MPD_lat = mul * w * w2 * (1 - E2) # meters per degree latitude
-    
-    return [MPD_long, MPD_lat]
+# PyGeodesy Github: https://github.com/mrJean1/PyGeodesy
+# Documentation: https://mrjean1.github.io/PyGeodesy/
+from pygeodesy.ellipsoidalVincenty import LatLon
+from pygeodesy import Datums
 
 def main():
     #Camera Specs
     focal = 0.002 # focal length in meters #x=sensor_hdimens*pixelsize_in_micrometers/1,000,000 #f = x/(2tan(angleofview/2))
     unitcell = 1.12 # size of single pixel in micrometers
-    resolution =  1/unitcell*1000000 # pixels per meter
+    resolution = 1/unitcell*1000000 # pixels per meter
     sensor_H = 4656 # pixel dimensions of camera sensor/photo
     sensor_V = 3496
     
     
     PATH = []
-    for i in [[0,0], [300,300]]:
+    for i in [[0,0],[4656/2-1,0],[4656-1,0],[4656-1,3496/2-1],[4656-1,3496-1],[4656/2-1,3496-1],[0,3496-1], [0,3496/2-1]]:
         testpixel = Node()
         testpixel.x = i[0]
         testpixel.y = i[1]
         PATH.append(testpixel)
-    print(PATH)
     
     # newNode = Node()
     # print(newNode)
@@ -67,43 +49,71 @@ def main():
     
 
     # image_center = Node()
-    # pixel_x_0 = int((len(img[0]))/2-1) # pixel x-coordinate 
     # pixel_y_0 = int((len(img))/2-1) # pixel y-coordinate
-    pixel_x_0 = int((sensor_H)/2-1) # pixel x-coordinate 
+    # pixel_x_0 = int((len(img[0]))/2-1) # pixel x-coordinate 
     pixel_y_0 = int((sensor_V)/2-1) # pixel y-coordinate
+    pixel_x_0 = int((sensor_H)/2-1) # pixel x-coordinate 
     
-    long_0 = -74.56913024979528 #longitude
     lat_0 = 40.61865173982036 #latitude
-    
+    long_0 = -74.56913024979528 #longitude
+    drone = LatLon(40.61865173982036, -74.56913024979528, datum=Datums.NAD83) # default datum is WGS-84
 
-    MPD_long = MPD(lat_0)[0]
-    MPD_lat = MPD(lat_0)[1]
+    # MPD_long = MPD(lat_0)[0]
+    # MPD_lat = MPD(lat_0)[1]
 
-    waypoints = []
+    rover_path = []
     
+    print("---")
+    
+    testingpath=[]
     for pixel in PATH:
-        distance_img_x = (pixel_x_0 - pixel.x) / resolution
-        distance_map_x = distance_img_x / magnification
-        print("horiz meters to center: " + str(distance_map_x))
-        long_1 = long_0 + distance_map_x / MPD_long
-        
         distance_img_y = (pixel_y_0 - pixel.y) / resolution
         distance_map_y = distance_img_y / magnification
-        print("vert meters to center: " + str(distance_map_y))
-        lat_1 = lat_0 + distance_map_y / MPD_lat
+        print("Vert meters to center: " + str(distance_map_y))
         
-        waypoints.append([lat_1, long_1])
+        distance_img_x = (pixel.x - pixel_x_0) / resolution
+        distance_map_x = distance_img_x / magnification
+        print("Horiz meters to center: " + str(distance_map_x))
+        
+        
+        bearing = 0 # compass 360 degrees; north = 0 degrees
+        if distance_map_x != 0:
+            bearing = 90 - (math.atan(distance_map_y/distance_map_x)*180/math.pi)
+            if distance_map_x < 0:
+                bearing += 180
+        elif distance_map_y < 0:
+            bearing = 180
+
+        distance_map = math.sqrt(distance_map_x**2 + distance_map_y**2)
+        
+        print("\nBearing: " + str(bearing))
+        print("Distance: " + str(distance_map) + "\n")
+        # http://www.movable-type.co.uk/scripts/latlong-vincenty.html
+        # Millimeter accuracy. To get to nanometer accuracy, will have to switch from Vincenty to Karney's method
+        waypoint = drone.destination(distance_map, bearing)
+        
+        rover_path.append([waypoint.lat, waypoint.lon])
+        testingpath.append(waypoint)
     
-    print(MPD_long)
-    print(MPD_lat)
+    print("Drone Coordinate: ")
+    print(drone)
     print([lat_0,long_0])
-    print(waypoints)
-    return waypoints
+    print("Path:")
+    for i in testingpath:
+        print(i)
+    print(rover_path)
+    return rover_path
 
 
-waypoint = main()
+ROVERPATH = main()
 
-lat, long = waypoint[0]
+print("---\nChecking Distance")
+
+drone = LatLon(40.61865173982036, -74.56913024979528, datum=Datums.NAD83)
+for i in range(0,len(ROVERPATH)):
+    waypoint = LatLon(ROVERPATH[i][0],ROVERPATH[i][1], datum=Datums.NAD83)
+    print(waypoint)
+    print(drone.distanceTo(waypoint))
 
 
 
