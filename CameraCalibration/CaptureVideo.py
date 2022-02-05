756707f7ace0f02832c76edce2dce9f34e5ff176
Charles0208 <cjiang0208@icloud.com>
2022-02-05 02:14:30 -0500

    Final video capturer for mac for camera calibration

--- CameraCalibration/CaptureVideo.py
+++ CameraCalibration/CaptureVideo.py
@@ -1,3 +1,5 @@
+# https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
+
 import numpy as np
 import cv2 as cv
 
@@ -15,15 +17,25 @@ if grabbed == False:
 else:
     print(f'Successful: {fwidth}(H) {fheight}(V)')
     
- 
 # Define the codec and create VideoWriter object
 # Windows
 # fourcc = cv.VideoWriter_fourcc(*'DIVX')
 # out = cv.VideoWriter('CameraCalibrationVideo.avi', fourcc, 20.0, (fwidth,fheight))
-
 # Mac
 fourcc = cv.VideoWriter_fourcc(*'MJPG')
-out = cv.VideoWriter('CameraCalibrationVideo1.mp4', fourcc, 20.0, (fwidth,fheight))
+out = cv.VideoWriter('CameraCalibrationVideo2.mp4', fourcc, 20.0, (fwidth,fheight))
 print("Video started...")
 while camera.isOpened():
     ret, frame = camera.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # write the frame
+    out.write(frame)
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == 27:
+        break
+# Release everything if job is finished
+camera.release()
+out.release()
+cv.destroyAllWindows()
