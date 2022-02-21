c700dc54259e87008eeb43ffab5aff5848e627ff
Charles0208 <cjiang0208@icloud.com>
2022-02-21 17:05:00 -0500

    master

--- CameraCalibration/Undistort.py
+++ CameraCalibration/Undistort.py
@@ -1,19 +1,10 @@
-#Undisort.py
-#Created by Chris Rillahan
-#Last Updated: 02/04/2015
-#Written with Python 2.7.2, OpenCV 2.4.8 and NumPy 1.8.0
+#Undistort.py
+#Last Updated: 02/21/2021
+#Last Updated with Python 3.7.0, OpenCV 4.5.4 and NumPy 1.21.5
 
-#This program takes a video file and removes the camera distortion based on the
-#camera calibration parameters.  The filename and the calibration data filenames
-#should be changed where appropriate.  Currently, the program is set to search for
-#these files in the folder in which this file is located.
-
-#This program first loads the calibration data.  Secondly, the video is loaded and
-#the metadata is derived from the file.  The export parameters and file structure
-#are then set-up.  The file then loops through each frame from the input video,
-#undistorts the frame and then saves the resulting frame into the output video.
-#It should be noted that the audio from the input file is not transfered to the
-#output file.
+#This program takes an image/images and removes the camera distortion based on the
+#camera calibration parameters. The filename and the calibration data filenames
+#should be changed where appropriate.
 
 import numpy as np
 import cv2, time, sys
@@ -30,7 +21,7 @@ class CameraParameters:
         img = cv2.undistort(img, self.intrinsic_matrix, self.distCoeff, None)
         return img
 
-    # read images from a folder and undistort each of them and write them as a new photo
+    # read images from a folder and undistort each of them and write them as new photos
     def undistortFolder(self, directory):
         print('Starting to undistort the photos....')
 
@@ -50,6 +41,14 @@ class CameraParameters:
 # camerainfo = CameraParameters()
 # camerainfo.undistortFolder(directory)
     
+    
+#This program first loads the calibration data.  Secondly, the video is loaded and
+#the metadata is derived from the file.  The export parameters and file structure
+#are then set-up.  The file then loops through each frame from the input video,
+#undistorts the frame and then saves the resulting frame into the output video.
+#It should be noted that the audio from the input file is not transfered to the
+#output file.
+
 #   # filename = 'GOPR0028.MP4'
 
 
--- PixelstoCoordinates.py
+++ PixelstoCoordinates.py
@@ -1,6 +1,5 @@
 # Converting pixels of the path into coordinates v2
 
-from multiprocessing import managers
 import cv2 as cv
 import numpy as np
 import glob
@@ -35,15 +34,14 @@ def main(PATH, directory):
     npz_calib_file = np.load('CameraCalibration/calibration_data.npz')
     intrinsic_matrix = npz_calib_file['intrinsic_matrix']
     unitcell = 3 # size of single pixel in micrometers
-    #focal = 0.0035 # focal length by taking an image and calculated necessary focal length to produce an accurate real world distance
     # AVG focal length from camera calibration
-    focal = (intrinsic_matrix[0][0]*unitcell + intrinsic_matrix[1][1]*unitcell)/2/1000000 # focal length in meters #x=sensor_hdimens*pixelsize_in_micrometers/1,000,000 #f = x/(2tan(angleofview/2))
+    focal = (intrinsic_matrix[0][0]*unitcell + intrinsic_matrix[1][1]*unitcell)/2/1000000 - 0.000117# focal length in meters #x=sensor_hdimens*pixelsize_in_micrometers/1,000,000 #f = x/(2tan(angleofview/2))
     resolution = 1/unitcell*1000000 # pixels per meter
     print("focal length: " + str(focal))
     
-    img = cv.imread(f'{directory}Distance_Testing-constantobjsize0.png')
+    img = cv.imread(f'{directory}Distance_Testing-constantobjsize1.png')
     sensor_H = img.shape[1] # pixel dimensions of camera sensor/photo (should be the same thing)
-    sensor_V = img.shape[0]
+    sensor_V = img.shape[0] # WILL JUST GET PIXEL DIMENSIONS FROM CALIBRATION_DATA.NPZ DURING CAMERA CALIBRATION
     
     print(sensor_H)
     print(sensor_V)
@@ -66,9 +64,6 @@ def main(PATH, directory):
     print("altitude: " + str(altitude))
     print("magnification: " + str(magnification))
     
-    # image_center = Node()
-    # pixel_y_0 = int((len(img))/2-1) # pixel y-coordinate
-    # pixel_x_0 = int((len(img[0]))/2-1) # pixel x-coordinate 
     pixel_y_0 = int((sensor_V)//2) # pixel y-coordinate
     pixel_x_0 = int((sensor_H)//2) # pixel x-coordinate 
     
@@ -80,51 +75,12 @@ def main(PATH, directory):
     
     print("---")
     
-    # TWO SETS OF PIXEL COORDINATES FOR CALCULATING DISTANCE
-    points1 = [(1200,845),(1022,659), (924,559), (870,502), (828,460), (807,436), (790,417)] # COMMENT OUT
-    pointsHome = [] # COMMENT OUT
-    for i in range(0,len(points1)):
-        pointsHome.append((pixel_x_0,points1[i][1]))
-    print(pointsHome)
-    # ALTITUDE FOR THE DIFFERENT DISTANCES
-    altitude = [1.774, 2.685, 3.595, 4.495, 5.421, 6.336, 7.25] # COMMENT OUT
-    # Finds the distance between any two points on an image and draws a line and adds 
-    for i in range(0,len(points1)):
-        image_dist = (altitude[i] * focal) / (altitude[i] - focal)
-        magnification = image_dist/altitude[i]
-        
-        print('Calculating distance between... ' + str(pointsHome[i]) + " and " + str(points1[i]))
-        distance_map_x, distance_map_y = distance(pointsHome[i], points1[i], resolution, magnification)
-        distance_map = math.sqrt(distance_map_x**2 + distance_map_y**2)
-        cv.line(img, pointsHome[i], points1[i], (0, 255, 0), 3)
-        
-        # displaying distance on image window
-        font = cv.FONT_HERSHEY_SIMPLEX
-        cv.putText(img, str(distance_map) + " meters-->",
-                   (pointsHome[i][0]-400, pointsHome[i][1]+5),
-                   font, 0.7, (255, 0, 0), 2)
-
-    cv.imshow('Distance between Points',img)
-    cv.imwrite(f'{directory}Distance_Testing-constantobjsize0-dist_measurements.png', img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
 
     # Finds physical x- y-distance to a point and returns the GPS coordinates
     for i in range(0,len(PATH)):
         pixelCenter = [pixel_x_0, pixel_y_0]
-        # image_dist = (altitude[i] * focal) / (altitude[i] - focal) # DELETE
-        # magnification = image_dist/altitude[i] # DELETE
         
         distance_map_x, distance_map_y = distance(pixelCenter, [PATH[i].x, PATH[i].y], resolution, magnification)
-        # distance_map_x, distance_map_y = distance(pixelCenter, [pixel.x, pixel.y], resolution, magnification)
-        
-        # distance_img_y = (pixel_y_0 - pixel.y) / resolution
-        # distance_map_y = distance_img_y / magnification
-        # print("Vert meters to center: " + str(distance_map_y))
-        
-        # distance_img_x = (pixel.x - pixel_x_0) / resolution
-        # distance_map_x = distance_img_x / magnification
-        # print("Horiz meters to center: " + str(distance_map_x))
         
         distance_map = math.sqrt(distance_map_x**2 + distance_map_y**2)
         
@@ -154,7 +110,7 @@ def main(PATH, directory):
 
 directory = 'CameraCalibration/pixeltocoordinate_imagetesting/'
 PATH = []
-img = cv.imread(f'{directory}Distance_Testing-constantobjsize0.png')
+img = cv.imread(f'{directory}Distance_Testing-constantobjsize1.png')
 sensor_H = img.shape[1] # pixel dimensions of camera sensor/photo (should be the same thing)
 sensor_V = img.shape[0]
 for i in [[0,0],[825,460],[sensor_H, sensor_V//2]]:
@@ -163,123 +119,3 @@ for i in [[0,0],[825,460],[sensor_H, sensor_V//2]]:
     testpixel.y = i[1]
     PATH.append(testpixel)
 ROVERPATH = main(PATH, directory)
-
-print("---\nChecking Distance")
-
-drone = LatLon(40.61865173982036, -74.56913024979528, datum=Datums.NAD83)
-for i in range(0,len(ROVERPATH)):
-    waypoint = LatLon(ROVERPATH[i][0],ROVERPATH[i][1], datum=Datums.NAD83)
-    print(waypoint)
-    print(drone.distanceTo(waypoint))
-
-
-
-# f = 500
-# rotXval = 90
-# rotYval = 90
-# rotZval = 90
-# distXval = 500
-# distYval = 500
-# distZval = 500
-
-# def onFchange(val):
-#     global f
-#     f = val
-# def onRotXChange(val):
-#     global rotXval
-#     rotXval = val
-# def onRotYChange(val):
-#     global rotYval
-#     rotYval = val
-# def onRotZChange(val):
-#     global rotZval
-#     rotZval = val
-# def onDistXChange(val):
-#     global distXval
-#     distXval = val
-# def onDistYChange(val):
-#     global distYval
-#     distYval = val
-# def onDistZChange(val):
-#     global distZval
-#     distZval = val
-
-# if __name__ == '__main__':
-
-#     #Read input image, and create output image
-#     src = cv.imread('CalibratingCamera/CheckerboardPhotos_1/checkerboard0.jpg')
-#     src = cv.resize(src,(640,480))
-#     dst = np.zeros_like(src)
-#     h, w = src.shape[:2]
-
-#     #Create user interface with trackbars that will allow to modify the parameters of the transformation
-#     wndname1 = "Source:"
-#     wndname2 = "WarpPerspective: "
-#     cv.namedWindow(wndname1, 1)
-#     cv.namedWindow(wndname2, 1)
-#     cv.createTrackbar("f", wndname2, f, 1000, onFchange)
-#     cv.createTrackbar("Rotation X", wndname2, rotXval, 180, onRotXChange)
-#     cv.createTrackbar("Rotation Y", wndname2, rotYval, 180, onRotYChange)
-#     cv.createTrackbar("Rotation Z", wndname2, rotZval, 180, onRotZChange)
-#     cv.createTrackbar("Distance X", wndname2, distXval, 1000, onDistXChange)
-#     cv.createTrackbar("Distance Y", wndname2, distYval, 1000, onDistYChange)
-#     cv.createTrackbar("Distance Z", wndname2, distZval, 1000, onDistZChange)
-
-#     #Show original image
-#     cv.imshow(wndname1, src)
-
-#     k = -1
-#     while k != 27:
-
-#         if f <= 0: f = 1
-#         rotX = (rotXval - 90)*np.pi/180
-#         rotY = (rotYval - 90)*np.pi/180
-#         rotZ = (rotZval - 90)*np.pi/180
-#         distX = distXval - 500
-#         distY = distYval - 500
-#         distZ = distZval - 500
-
-#         # Camera intrinsic matrix
-#         K = np.array([[f, 0, w/2, 0],
-#                     [0, f, h/2, 0],
-#                     [0, 0,   1, 0]])
-
-#         # K inverse
-#         Kinv = np.zeros((4,3))
-#         Kinv[:3,:3] = np.linalg.inv(K[:3,:3])*f
-#         Kinv[-1,:] = [0, 0, 1]
-
-#         # Rotation matrices around the X,Y,Z axis
-#         RX = np.array([[1,           0,            0, 0],
-#                     [0,np.cos(rotX),-np.sin(rotX), 0],
-#                     [0,np.sin(rotX),np.cos(rotX) , 0],
-#                     [0,           0,            0, 1]])
-
-#         RY = np.array([[ np.cos(rotY), 0, np.sin(rotY), 0],
-#                     [            0, 1,            0, 0],
-#                     [ -np.sin(rotY), 0, np.cos(rotY), 0],
-#                     [            0, 0,            0, 1]])
-
-#         RZ = np.array([[ np.cos(rotZ), -np.sin(rotZ), 0, 0],
-#                     [ np.sin(rotZ), np.cos(rotZ), 0, 0],
-#                     [            0,            0, 1, 0],
-#                     [            0,            0, 0, 1]])
-
-#         # Composed rotation matrix with (RX,RY,RZ)
-#         R = np.linalg.multi_dot([ RX , RY , RZ ])
-
-#         # Translation matrix
-#         T = np.array([[1,0,0,distX],
-#                     [0,1,0,distY],
-#                     [0,0,1,distZ],
-#                     [0,0,0,1]])
-
-#         # Overall homography matrix
-#         H = np.linalg.multi_dot([K, R, T, Kinv])
-
-#         # Apply matrix transformation
-#         cv.warpPerspective(src, H, (w, h), dst, cv.INTER_NEAREST, cv.BORDER_CONSTANT, 0)
-
-#         # Show the image
-#         cv.imshow(wndname2, dst)
-#         k = cv.waitKey(1)
